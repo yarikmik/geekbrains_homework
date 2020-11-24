0262e815c1f29dd41c2bfe47ef330c8bab1011fd
Nikolay <yarik.mik@gmail.com>
2020-11-25 04:37:20 +0700

    task_7.1 completed

--- lesson_7/task_7.1.py
+++ lesson_7/task_7.1.py
@@ -18,26 +18,37 @@ class Matrix:
     def __str__(self):
         return '\n'.join(['   '.join([str(i) for i in a]) for a in self.matrix]) + '\n'
 
-
     def __add__(self, other):
-        matrix1 = self.matrix
-        matrix2 = other
-        if len(matrix1) != len(matrix2):
-            return 'Массивы разной длинны'
-        for m1_str in self.matrix1:
+        if len(self.matrix) != len(other):
+            return print('Массивы разной длинны')
+        for m1_str in self.matrix:
             for m2_str in other:
                 if len(m1_str) != len(m2_str):
-                    return 'В строках разное колличество значений'
+                    return print('В строках разное колличество значений')
+
+        # Сложение массивов:
+        summ_arr = []
+        for i in range(len(self.matrix)):
+            summ = []
+            for s in range(len(self.matrix[i])):
+                summ.append(self.matrix[i][s] + other[i][s])
+            summ_arr.append(summ)
+        return Matrix(summ_arr)
 
 
-list1 = [[3, 3, 5], [2, 5, 7], [1, 3, 0]]
-list2 = [[1, 1, 1], [2, 2, 2], [3, 3]]
+matrix1 = Matrix([[3, 3, 5], [2, 5, 7], [1, 3, 0]])
+matrix2 = Matrix([[1, 1, 1], [2, 2, 2], [2, 2]])
+matrix3 = Matrix([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
+list1 = [[0, 1, 1], [2, 1, 2]]
 
-matrix1 = Matrix(list1)
-matrix2 = Matrix(list2)
-matrix1 + matrix2
-print(matrix2.matrix)
 print(matrix1)
 print(matrix2)
+print(matrix3)
+
+print(f'Неудачное сложение: {matrix1 + matrix2.matrix}\n')
+print(f'Неудачное сложение: {matrix1 + list1}\n')
+print(f'Удачное сложение: \n{matrix1 + matrix3.matrix}\n')
+
+
 
 
